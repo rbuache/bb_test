be4358f0ac5d5dfc8d6583239908fac5bdadf177
rbuache <rbuache@gmail.com>
2025-02-17 14:27:19 +0100

    Update ci.py

--- ci.py
+++ ci.py
@@ -1,7 +1,7 @@
 import os
 
 cmd1 = "{\"name\":\"Print current path\", \"cmd\":\"pwd\"}"
-cmd2 = "{\"name\":\"Print current path\", \"cmd\":\"cat ci.py\"}"
+cmd2 = "{\"name\":\"print ci file\", \"cmd\":\"cat ci.py\"}"
 
 print(cmd1)
 print(cmd2)
