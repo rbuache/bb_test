8c15b38c682358d286de24be78b22b6a2c263765
rbuache <rbuache@gmail.com>
2025-02-17 15:06:14 +0100

    Update ci.py

--- ci.py
+++ ci.py
@@ -1,8 +1,15 @@
 import os
+import json
 
-cmd1 = '{"name":"Print current path", "cmd":"pwd"}'
-cmd2 = '{"name":"print ci file", "cmd":"cat ci.py"}'
+class ShellStep():
+  def __init__(self, name, cmd):
+    self.name = name
+    self.cmd = cmd
+    self.type = "shell"
+  def __str__(self):
+     return json.dumps(self.__dict__) 
+s1 = ShellStep("Print current path", "pwd")
+s2 = ShellStep("print ci file", "cat ci.py")
 
-a =4215
-print(cmd1)
-print(cmd2)
+print(s1)
+print(s2)
